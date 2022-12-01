e7cc006d79b52f9c4fe376323f5e45267ad13fcd
Arnaud WIEREL <arnaud.wierel@viacesi.fr>
2022-12-02 00:21:03 +0100

    creation page jeu

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import HomePage from './components/HomePage.vue'
+import HomePage from "./components/HomePage.vue";
+import Jeux from "./components/Jeux.vue";
 </script>
 
 <template>
   <HomePage />
+  <Jeux />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ vue-project/src/components/Jeux.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="jeux">
+    <div class="jeux__title">
+      <h1>Jeux</h1>
+    </div>
+    <div class="jeux__button">
+      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Jouer</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Jeux",
+};
+</script>
+
+<style>
+.jeux {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+}
+
+.jeux__title {
+  font-size: 5rem;
+  font-weight: 700;
+  color: #fff;
+  margin-bottom: 50px;
+}
+
+.jeux__button a {
+  padding: 20px;
+  border-radius: 0.5rem;
+  font-size: 30px;
+  background-color: rgb(0 189 126 / 0.5);
+  color: #fff;
+}
+
+.jeux__button {
+  transition: all 0.3s ease;
+}
+
+.jeux__button:hover {
+  transform: scale(1.5);
+}
+
+.jeux__button a:hover {
+  background-color: rgb(0 189 126);
+}
+</style>
