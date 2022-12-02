7970c52fc9c5ec4273cd57cf138ee5087d92eec8
Acegenesis <66308616+Acegenesis@users.noreply.github.com>
2022-12-02 01:55:09 +0100

    nono

--- vue-project/src/components/Jeux.vue
+++ vue-project/src/components/Jeux.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="jeux">
-    <div class="jeux__title">
-      <h1>Jeux</h1>
-    </div>
-    <div class="jeux__button">
-      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Jouer</a>
+    <div class="cards my-cards">
+
     </div>
   </div>
 </template>
@@ -17,38 +14,20 @@ export default {
 
 <style>
 .jeux {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
   width: 100vw;
   height: 100vh;
+  position: relative;
 }
 
-.jeux__title {
-  font-size: 5rem;
-  font-weight: 700;
-  color: #fff;
-  margin-bottom: 50px;
-}
-
-.jeux__button a {
-  padding: 20px;
-  border-radius: 0.5rem;
-  font-size: 30px;
-  background-color: rgb(0 189 126 / 0.5);
-  color: #fff;
-}
-
-.jeux__button {
-  transition: all 0.3s ease;
-}
-
-.jeux__button:hover {
-  transform: scale(1.5);
-}
-
-.jeux__button a:hover {
-  background-color: rgb(0 189 126);
+.cards {
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 50%;
+  height: 150px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  background-color: #7C96BC;
 }
 </style>
